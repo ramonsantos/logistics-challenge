ea40e7f42e60ae5bfd7f70c87e8c0b20e6841979
ramonsantos <ramonsantos.pe@gmail.com>
2024-11-03 18:49:47 -0300

    Add associations

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,5 +5,6 @@ require 'rails_helper'
 RSpec.describe Order, type: :model do
   context 'associations' do
     it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_many(:order_products).dependent(:destroy) }
   end
 end
